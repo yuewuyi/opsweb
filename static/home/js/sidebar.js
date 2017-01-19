9ff29b2ffe8f37ee1d3e25c19a5293af533ccca9
yuewuyi <657415815@qq.com>
2017-01-19 23:16:03 +0800

    父选项重复显示

--- static/home/js/sidebar.js
+++ static/home/js/sidebar.js
@@ -1,3 +1,10 @@
 /**
  * Created by suyue on 2017/1/18.
  */
+function mask(action,mask_id){
+    if (action=="over"){
+        $("#"+mask_id).css("width","100%")
+    }else if(action=="out"){
+        $("#"+mask_id).css("width","3px")
+    }
+}
