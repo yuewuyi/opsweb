484f3f838cdd6bdd7d9608e04661c14ff6f53e0e
yuewuyi <657415815@qq.com>
2017-03-01 18:07:31 +0800

    报警消息显示

--- utils/zabbix_public_invok.py
+++ utils/zabbix_public_invok.py
@@ -6,8 +6,17 @@ class zabbix_data:
         conf=app_config()
         self.__config_value=conf.get_config_value()
     def host_trigger_get(self,params):
+        warn=[]
+        disable=[]
+        enable=[]
         zapi = ZabbixAPI(url=self.__config_value['zabbix_api_addr'][0], user=self.__config_value['zabbix_api_user'][0],password=self.__config_value['zabbix_api_user'][1])
         result = zapi.do_request('host.get',params)['result']
         for i in range(len(result)):
-            result[i]['triggers'] = list(filter(lambda x: x['value'] == "1" or x['status'] == 1, result[i]['triggers']))
-        return result
+            result[i]['triggers'] = list(filter(lambda x: x['value'] == "1" and x['status'] == "0", result[i]['triggers']))
+            if result[i]['status']=='1':
+                disable.append(result[i])
+            elif result[i]['triggers']:
+                warn.append(result[i])
+            else:
+                enable.append(result[i])
+        return warn+enable+disable
