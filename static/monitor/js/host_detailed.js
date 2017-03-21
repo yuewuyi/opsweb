f0663b191d2da715b88f884b42971a960b5a6933
yuewuyi <657415815@qq.com>
2017-03-21 18:10:17 +0800

    磁盘饼图

---
+++ static/monitor/js/host_detailed.js
@@ -0,0 +1,45 @@
+// /**
+//  * Created by suyue on 2017/3/21.
+//  */
+// $(function () {
+//     $('#g1').highcharts({
+//         chart: {
+//             margin:[0,0,0,0],
+//             spacing:[0,0,0,0],
+//         },
+//         title: {
+//             text: '/',
+//             margin:[0,0,0,0],
+//             style:{
+//                 fontSize:5,
+//             }
+//         },
+//         tooltip: {
+//             headerFormat: '{series.name}<br>',
+//             pointFormat: '{point.name}: <b>{point.percentage:.1f}%</b>'
+//         },
+//         plotOptions: {
+//             pie: {
+//                 allowPointSelect: true,
+//                 cursor: 'pointer',
+//                 dataLabels: {
+//                     enabled: false
+//                 },
+//             },
+//         },
+//         exporting:{
+//                 enabled:false
+//             },
+//         series: [{
+//             type: 'pie',
+//             name: '浏览器访问量占比',
+//             data: [
+//                 ['Firefox',45.0],
+//                 ['IE',26.8]
+//             ]
+//         }],
+//         credits: {
+//                 enabled: false
+//             },
+//     });
+// });
